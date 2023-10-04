ecb02104f28d6a20b0377f9a47a805e1fe43c956
kreby455 <clebinhoalves2011@gmail.com>
2023-10-04 09:14:23 -0300

    Register

---
+++ src/components/Button/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button as ContainerButton} from './styles'
+
+function Button({children, ...rest}){
+
+    return <ContainerButton{...rest}>{children}</ContainerButton>
+}
+
+export default Button
